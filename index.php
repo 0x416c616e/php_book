34db7440458d9b9c33addfcf8f8c6ed4029290d8
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2019-11-30 08:00:16 -0600

    got rid of default bitnami html stuff and made an index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+	<title>WAMP stack</title>
+</head>
+<body>
+	<p>Here are some things you can do in this project:</p>
+	<ul>
+		<li><a href="/phpmyadmin">phpMyAdmin</a></li>
+		<li><a href="/phpinfo.php">phpinfo</a></li>
+	</ul>
+	<?php echo "hello"; ?>
+</body>
+</html>
